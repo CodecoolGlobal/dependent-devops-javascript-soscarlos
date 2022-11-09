d66fe68ac1c53faecf36986d3a92fc75ee867b1a
carlos <soscarlos@gmail.com>
2022-11-09 10:53:47 +0100

    create testing workflow with jest unit test

---
+++ index.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+
+const {app, server} = require('./index');
+
+describe("test main express message", () => {
+    test('responds to /', async () => {
+        const response = await request(app).get('/');         
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('Hello World!\n');
+        server.close();
+    });
+});
